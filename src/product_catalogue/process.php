e49f596ea8efacfd2dea2ccf594c53849b289739
Thomas Walsh <lwalsh7@luc.edu>
2020-11-30 19:02:39 -0600

    some broken shit

---
+++ src/product_catalogue/process.php
@@ -0,0 +1,22 @@
+<?php include 'database.php'; ?>
+
+<?php
+
+// create a variable
+$title=$_POST['title'];
+$description=$_POST['desc'];
+$category=$_POST['category'];
+$price=$_POST['price'];
+$user=$_POST['user'];
+
+//Execute the query
+mysqli_query($connect,"INSERT INTO products (title, desc, category, price, user)
+		        VALUES ('$title','$desc','$category','$price', '$user')");
+				
+	if(mysqli_affected_rows($connect) > 0){
+	echo "<p>Product Added</p>";
+	echo "<a href="index.html">Go Back</a>";
+} else {
+	echo "Product NOT Added<br />";
+	echo mysqli_error ($connect);
+}
